c16c996f8f5f3d1d312210b830a098640acf7b49
Sergio Schvezov <sergio.schvezov@canonical.com>
2022-02-17 10:42:19 -0300

    errors: new module
    Mostly imported from the original Snapcraft code base with updates for pylint
    and other linter compliance.
    
    Tests were also added.
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

---
+++ craft_grammar/errors.py
@@ -0,0 +1,55 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2022 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Errors for Craft Grammar."""
+
+
+class CraftGrammarError(Exception):
+    """Base class error for craft-grammar."""
+
+
+class GrammarSyntaxError(CraftGrammarError):
+    """Error raised on grammar syntax errors."""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(f"Invalid grammar syntax: {message}.")
+
+
+class OnStatementSyntaxError(GrammarSyntaxError):
+    """Error raised on on statement syntax errors."""
+
+    def __init__(self, on_statement, *, message=None):
+        components = [f"{on_statement!r} is not a valid 'on' clause"]
+        if message:
+            components.append(message)
+        super().__init__(message=": ".join(components))
+
+
+class ToStatementSyntaxError(GrammarSyntaxError):
+    """Error raised on to statement syntax errors."""
+
+    def __init__(self, to_statement, *, message=None):
+        components = [f"{to_statement!r} is not a valid 'to' clause"]
+        if message:
+            components.append(message)
+        super().__init__(message=": ".join(components))
+
+
+class UnsatisfiedStatementError(CraftGrammarError):
+    """Error raised when a statement cannot be satisfied."""
+
+    def __init__(self, statement):
+        super().__init__(f"Unable to satisfy {statement!r}, failure forced.")
---
+++ tests/unit/test_errors.py
@@ -0,0 +1,50 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2022 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import pytest
+
+from craft_grammar import errors
+
+scenarios = (
+    {
+        "exception_class": errors.GrammarSyntaxError,
+        "args": ["syntax incorrect"],
+        "expected_message": "Invalid grammar syntax: syntax incorrect.",
+    },
+    {
+        "exception_class": errors.OnStatementSyntaxError,
+        "args": ["syntax incorrect"],
+        "expected_message": "Invalid grammar syntax: 'syntax incorrect' is not a valid 'on' clause.",
+    },
+    {
+        "exception_class": errors.ToStatementSyntaxError,
+        "args": ["syntax incorrect"],
+        "expected_message": "Invalid grammar syntax: 'syntax incorrect' is not a valid 'to' clause.",
+    },
+    {
+        "exception_class": errors.UnsatisfiedStatementError,
+        "args": ["foo bar"],
+        "expected_message": "Unable to satisfy 'foo bar', failure forced.",
+    },
+)
+
+
+@pytest.mark.parametrize("scenario", scenarios)
+def test_error_formatting(scenario):
+    assert str(scenario["exception_class"](*scenario["args"])) == (
+        scenario["expected_message"]
+    )
